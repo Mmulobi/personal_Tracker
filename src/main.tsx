ce641bfee0e6e24d0f11615e517d0bc041e5ceac
Mmulobi <mosesmutush7@gmail.com>
2024-12-19 08:35:30 +0300

    fix display rendering

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,11 @@ import App from './App.tsx'
 import './index.css'
 
 // Optional Vercel Analytics import
-try {
-  const { Analytics } = require('@vercel/analytics/react');
-  
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-      <Analytics />
-    </React.StrictMode>,
-  )
-} catch (error) {
-  // If Vercel Analytics is not available, render without it
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
-}
+import { Analytics } from '@vercel/analytics/react';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+    <Analytics />
+  </React.StrictMode>,
+)
